1c5e69e1cdb4573ff836d49b19d926636d238151
michael <mishaba1990@gmail.com>
2022-02-25 11:27:06 +0200

    achanged api url to prod

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL = "http://localhost:8000/api/test/";
-const API_URL = "https://jam-alert-back.herokuapp.com/";
+const API_URL = "https://jam-alert-back.herokuapp.com/api/test/";
 
 
 const getPublicContent = () => {
